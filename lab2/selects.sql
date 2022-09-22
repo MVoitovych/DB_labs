ed30d59b7abfcf81a3657c61aae6946d8abc7993
Mykhailo Voitovych <m.voitovych21@gmail.com>
2022-09-23 00:21:59 +0300

    added tasks 7,9

--- lab2/selects.sql
+++ lab2/selects.sql
@@ -29,23 +29,34 @@ USE Labor_SQL;
 -- 6
 -- SELECT DATE_FORMAT(date, '%Y.%m.%d') as date FROM battles;
 
--- 7 coming soon
+-- 7 
+-- SELECT country, battle, count(country) AS num_of_ships FROM 
+-- ships JOIN classes ON ships.class = classes.class
+-- JOIN outcomes ON ships.NAME = outcomes.ship
+-- GROUP BY country, battle
+-- HAVING num_of_ships >= 2;
  
  
 -- 8
 -- SELECT name, result, country, numGuns FROM 
-
 -- (SELECT * from ships JOIN 
 -- (SELECT * FROM outcomes where result = 'damaged')
 -- ON ships.name = outcomes.ship) AS ships_result
 -- JOIN classes ON classes.class = ships_result.class;
 
--- 9 coming soon
- 
+-- 9 
+-- SELECT DISTINCT product.maker, 
+-- CASE 
+-- 	WHEN count IS NOT NULL THEN concat('Yes (', count, ')')
+-- 	ELSE 'no'
+-- END AS printer 
+-- FROM product LEFT JOIN
+-- (SELECT DISTINCT maker, count(maker) AS count FROM 
+-- product WHERE TYPE = 'Printer' GROUP BY maker) AS tabl
+-- ON product.maker = tabl.maker
 
 -- 10
 -- SELECT AVG(price) FROM
-
 -- (SELECT price, model FROM pc 
 -- UNION
 -- SELECT price, model FROM laptop) AS pc_laptop
@@ -53,6 +64,9 @@ USE Labor_SQL;
 -- (SELECT model FROM product
 -- WHERE maker = 'A' AND (type = 'PC' OR type = 'Laptop')) AS product_model
 -- ON pc_laptop.model = product_model.model;
+
+
+
 
 
 
